8987bb62fcbc62798f6b1c0bea1a2164d992b88e
Dot-KT <7devanle@gmail.com>
2026-09-07 09:19:14 +0100

    add done

--- Index.js
+++ Index.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-const student = new Student("James Chad", "Dotkt", 12, "male", "Ogun", "Yola", "OAU", "English", "300Level", ["Flying kite", "gaming", "writing"], ["butchering", "gaming"], "Yam", "Gray", "Just be Chairman", "From the street", "click link to view profile photo=> localhost:3000/images/image.png", "x.com/profile/Dot_kt", "08063211050");
+const student = new Student("James Chad", "Dotkt", 12, "male", "Ogun", "Yola", "OAU", "English", "300Level", ["Flying kite", "gaming", "writing"], ["butchering", "gaming"], "Yam", "Gray", "Just be Chairman", "From the street", "click link to view profile photo=> localhost:3000/images/image.png", "x.com/profile/Dot__kt", "08063211050");
 
 app.get('/api/home/profilePage', (req, res)=> {
     res.status(200).send("Welcome to Personal profile");
